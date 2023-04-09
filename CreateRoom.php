fef094c2003dc61a665fa3452c4b290bc640abec
IRAFRIX <110074200+IRAFRIX@users.noreply.github.com>
2023-04-09 21:55:47 +0700

    atcreateroomsuccess

--- CreateRoom.php
+++ CreateRoom.php
@@ -10,42 +10,41 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CreateRoom</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <script>
-
-		function password_show_hide(){
-            let x=document.getElementById("password");
-            let show_eye=document.getElementById("show_eye");
-            let hide_eye=document.getElementById("hide_eye");
-            hide_eye.classList.remove("d-none");
-            if(x.type==="password"){
-                x.type="text";
-                show_eye.style.display="none";
-                hide_eye.style.display="block";
-            }else{
-                x.type="password";
-                show_eye.style.display="block";
-                hide_eye.style.display="none";
+		function password_show_hide() {
+            let x = document.getElementById("password");
+            let show_eye = document.getElementById("show_eye");
+            let hide_eye = document.getElementById("hide_eye");
+            hide_eye.classList.remove("is-hidden");
+            if(x.type === "password"){
+                x.type = "text";
+                show_eye.style.display = "none";
+                hide_eye.style.display = "block";
+            } else {
+                x.type = "password";
+                show_eye.style.display = "block";
+                hide_eye.style.display = "none";
             }
         }
 		
 		function createPassword() {
             var passwordInputBlock = document.getElementById("password-input-block");
-			passwordInputBlock.style.display = "block";
+			passwordInputBlock.classList.remove("is-hidden");
 		}
 		
 		function closePassword() {
             var passwordInputBlock = document.getElementById("password-input-block");
-			passwordInputBlock.style.display = "none";
+			passwordInputBlock.classList.add("is-hidden");
 		}
 		
 		function savePassword() {
 			var passwordInput = document.getElementById("password-input");
 			var password = passwordInput.value;
-			if (password != "") {
+			if (password != "rpwd") {
 				// Save the password to a database or use it in your application
 				alert("Your password has been saved!");
 				passwordInput.value = "";
@@ -53,26 +52,51 @@
 			}
 		}
 	</script>
-    <title>CreateRoom</title>
 </head>
 <body>
-<!--ใส่ข้อมูล ชื่อห้องแชท-->
-<div class="card text-dark bg-white border-primary">
-                <div class="card-header bg-primary text-white">กรอกข้อมูลห้องแชท</div>
-                <div class="card-body">
-                    <!--<form action="register_save.php" method="post">-->
-                        <div class="row mb-2">
-                            <label class="col-mb-3 col-form-label">ชื่อห้องแชท</label>
-                            <input type="text" name="rn" class="form-control" require>
+    <!--ใส่ข้อมูล ชื่อห้องแชท-->
+    <section class="section">
+        <div class="block">
+            <div class="container">
+                <div class="card">
+                    <header class="card-header has-background-primary has-text-white">
+                        <p class="card-header-title">
+                            กรอกข้อมูลห้องแชท
+                        </p>
+                    </header>
+                    <form action="createroomsucces.php" method="get">
+                    <div class="card-content">
+                        <div class="field">
+                            <label class="label">ชื่อห้องแชท</label>
+                            <div class="control">
+                                <input type="text" name="rn" class="input" required>
+                            </div>
+                        
+                    <!--ปุ่มเปิด-ปิด Private-->
+                    <div class="field is-grouped mt-5">
+                    <div class="control">
+                        <button class="button is-success" onclick="closePassword()">Public</button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-danger" onclick="createPassword()">Private</button>
+                        <div id="password-input-block" class="is-hidden mt-3">
+                            <input type="password" id="password-input" class="input mb-3" placeholder="Enter your new password">
+                            <button class="button is-primary mr-3" onclick="savePassword()">Save Password</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="field is-grouped mt-5">
+                            <div class="control">
+                                <button type="submit" class="button is-primary">Submit</button>
+                            </div>
+                            <div class="control">
+                                <button type="reset" class="button is-light">Reset</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-<!--ปุ่มเปิด-ปิด Private-->
-    <div class="container mt-5">
-        <button class="btn btn-success" onclick="closePassword()">Public</button>
-		<button class="btn btn-danger" onclick="createPassword()">Private</button>
-		<div id="password-input-block" class="mt-3" style="display: none;">
-			<input type="password" id="password-input" class="form-control mb-3" placeholder="Enter your new password">
-			<button class="btn btn-primary mr-3" onclick="savePassword()">Save Password</button>
-		</div>
-	</div>
-</body>
-</html>
