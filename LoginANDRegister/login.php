f80c1a8d36f9207304da07297dda996279458555
Nwpatt <110074022+Nwpatt@users.noreply.github.com>
2023-04-11 10:21:00 +0700

    Update login.php

--- LoginANDRegister/login.php
+++ LoginANDRegister/login.php
@@ -13,62 +13,75 @@ if (isset($_SESSION['id'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <style>
+        .bg-image {
+            width: 100%;
+            height: 900px;
+            background-image: url('../bg.jpg');
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+        }
+    </style>
     <title>Login</title>
-     <!-- <link rel="icon" href="logo.jpg" type="image/icon type"> -->
+    <!-- <link rel="icon" href="logo.jpg" type="image/icon type"> -->
 </head>
 
 <body>
-    <div class="container">
-        <img src="../header.jpg">
-    </div>
-    <div class="container">
-        <h1 class="title has-text-centered">PigPig</h1>
-        <br>
-        <div class="columns">
-            <div class="column is-4"></div>
-            <div class="column is-4">
-                <?php
-                if (isset($_SESSION['error'])) {
-                    echo "<div>
+    <div class="bg-image">
+        <div class="container">
+            <img src="../header.jpg">
+        </div>
+        <div class="container">
+
+            <br>
+            <div class="columns">
+                <div class="column is-4"></div>
+                <div class="column is-4">
+                    <?php
+                    if (isset($_SESSION['error'])) {
+                        echo "<div>
                 ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง</div>";
-                    unset($_SESSION['error']);
-                }
-                ?>
-                <div class="card">
-                    <div class="has-text-centered">เข้าสู่ระบบ</div>
-                    <div class="columns is-centered is-vcentered is-multiline is-mobile">
-                        <form action="verify.php" method="post">
-                            <div class="columns my-3 has-text-centered">
-                                <div class="column is-half">
-                                    User ID
+                        unset($_SESSION['error']);
+                    }
+                    ?>
+                    <div class="card">
+                    <header class="card-header has-background-black">
+                            <p class="card-header-title has-text-white">เข้าสู่ระบบ</p>
+                        </header>
+                        <div class="columns is-centered is-vcentered is-multiline is-mobile">
+                            <form action="verify.php" method="post">
+                                <div class="columns my-3 has-text-centered">
+                                    <div class="column is-half">
+                                        User ID
+                                    </div>
+                                    <div class="column is-half">
+                                        <input type="text" name="login" class="form-control">
+                                    </div>
                                 </div>
-                                <div class="column is-half">
-                                    <input type="text" name="login" class="form-control">
+                                <div class="columns my-3 has-text-centered">
+                                    <div class="column is-half">
+                                        Password
+                                    </div>
+                                    <div class="column is-half">
+                                        <input type="password" name="pwd" class="form-control" id="password">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="columns my-3 has-text-centered">
-                                <div class="column is-half">
-                                    Password
-                                </div>
-                                <div class="column is-half">
-                                    <input type="password" name="pwd" class="form-control" id="password">
-                                </div>
-                            </div>
-                            <center>
-                                <div class="column">
-                                    <button type="submit" class="button is-primary is-small">เข้าสู่ระบบ</button>
-                                </div>
-                            </center>
-                        </form>
+                                <center>
+                                    <div class="column">
+                                        <button type="submit" class="button is-dark is-small">เข้าสู่ระบบ</button>
+                                    </div>
+                                </center>
+                            </form>
+                        </div>
                     </div>
                 </div>
+                <div class="column is-4"></div>
             </div>
-            <div class="column is-4"></div>
+            <br>
+            <p align="center">ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a></p>
+            <br>
+            <p align="center"><a href="../homepage.php">กลับสู่หน้าหลัก</a></p>
         </div>
-        <br>
-        <p align="center">ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a></p>
-        <br>
-        <p align="center"><a href="../homepage.php">กลับสู่หน้าหลัก</a></p>
     </div>
 </body>
 
